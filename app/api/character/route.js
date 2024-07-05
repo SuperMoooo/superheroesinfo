467aa2057588ef5d6a0b70a9100a32f45153ec38
SuperMoooo <andremontoito@gmail.com>
2024-07-05 13:46:45 +0100

    Update - Fix API for Static Publish

--- app/api/character/route.js
+++ app/api/character/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-
+export const dynamic = 'force-dynamic';
 export async function GET(request) {
     const url = new URL(request.url);
     const search = url.searchParams.get('search');
