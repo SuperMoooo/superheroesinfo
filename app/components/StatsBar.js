3c0f92243571f645214617b6c9fc3d9634f74850
SuperMoooo <andremontoito@gmail.com>
2024-07-09 16:58:14 +0100

    Update - Fix StatsBar

--- app/components/StatsBar.js
+++ app/components/StatsBar.js
@@ -2,23 +2,27 @@ import React from 'react';
 
 export default function StatsBar({ title, value }) {
     return (
-        <li className="w-full flex flex-row  items-center space-x-4">
-            <h1
-                className={`uppercase text-xl font-bold w-36 ${
-                    value === 'null' && 'mr-7'
-                }`}
-            >
-                {title}
-            </h1>
-            <div
-                style={{
-                    width: value === 'null' ? '0%' : parseInt(value) + '%',
-                }}
-                className={` h-full bg-yellow-600 flex justify-end items-center`}
-            >
-                <h2 className="mr-2 font-bold">
-                    {value == 'null' ? '???' : value}
-                </h2>
+        <li className="w-full flex flex-row items-center justify-start space-x-4 ">
+            <div className="w-48">
+                <h1
+                    className={`uppercase  font-bold md:text-xl text-lg ${
+                        value === 'null' && 'mr-7'
+                    }`}
+                >
+                    {title}
+                </h1>
+            </div>
+            <div className="md:w-full w-2/3">
+                <div
+                    style={{
+                        width: value === 'null' ? '0%' : parseInt(value) + '%',
+                    }}
+                    className={` h-full bg-yellow-600 flex justify-end  `}
+                >
+                    <h2 className="mr-2 font-bold">
+                        {value == 'null' ? '???' : value}
+                    </h2>
+                </div>
             </div>
         </li>
     );
